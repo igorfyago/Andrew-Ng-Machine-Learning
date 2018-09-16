af48fd5efe70ac5fb7255556a77bc313aa5a16e0
Igor F. Yago <igor.yago@gmail.com>
2018-09-16 20:20:33 +0100

    scala

---
+++ Scala/build.sbt
@@ -0,0 +1,5 @@
+name := "HelloWorld"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
---
+++ Scala/src/main/scala/com/tryScala/HelloWorld.scala
@@ -0,0 +1,7 @@
+package com.tryScala
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
